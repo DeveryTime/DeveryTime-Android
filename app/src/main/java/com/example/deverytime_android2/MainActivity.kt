93cce6d6f3da0ba303d1a12f5c4a5eadbfd12abe
chogunhwy <gun979999@gmail.com>
2026-07-27 14:56:01 +0900

    feat: email input

--- app/src/main/java/com/example/deverytime_android2/MainActivity.kt
+++ app/src/main/java/com/example/deverytime_android2/MainActivity.kt
@@ -1,16 +1,30 @@
 package com.example.deverytime_android2
 
+import android.R
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.deverytime_android2.ui.theme.DeveryTime_Android2Theme
 
 class MainActivity : ComponentActivity() {
@@ -21,7 +35,7 @@ class MainActivity : ComponentActivity() {
             DeveryTime_Android2Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        schoolmail = "이메일",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -29,19 +43,46 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
-
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun Greeting(
+    schoolmail: String,
+    modifier: Modifier = Modifier
+){
+Column (modifier = modifier.padding(16.dp,end = 16.dp, top = 220.dp).fillMaxWidth()) {
+
+    var schoolmailreciver by remember { mutableStateOf("") }
+
     Text(
-        text = "Hello $name!",
+        text = "이메일",
+        color = Color(0xFF999999)
+        )
+    OutlinedTextField(
+        colors = OutlinedTextFieldDefaults.colors(
+            // 1. 입력한 글자 색상
+            focusedTextColor = Color.Black,     // 클릭(포커스)했을 때 글자 색
+            unfocusedTextColor = Color.Black,   // 기본 상태 글자 색
+
+            // 2. 안내글자(Placeholder) 색상
+            focusedPlaceholderColor = Color.Transparent, // 클릭하면 안내글자 숨기기
+            unfocusedPlaceholderColor = Color(0xFF999999), // 기본 상태 안내글자 색
+
+            // 3. 에러 났을 때 색상
+            errorBorderColor = Color.Red,
+        ),
+        placeholder = { Text(text = "이메일")},
+        value = schoolmailreciver,
+        onValueChange = { schoolmailreciver = it },
         modifier = modifier
-    )
+            .fillMaxWidth(),
+        shape = RoundedCornerShape(12.dp),
+        )
+    }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, device = "id:pixel_4", showSystemUi = true)
 @Composable
 fun GreetingPreview() {
     DeveryTime_Android2Theme {
-        Greeting("Android")
+        Greeting("")
     }
 }
