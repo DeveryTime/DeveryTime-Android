7f86a64f3c961d5ed3823c333e1e69e645827a85
Jin Hwang <wlsghkd0921@gmail.com>
2026-09-18 15:18:53 +0900

    build: Retrofit 라이브러리와 Gson변환기 라이브러리 의존성 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,5 +56,7 @@ dependencies {
     implementation("androidx.navigation:navigation-compose:2.8.0")
     implementation("androidx.compose.material:material-icons-extended")
     implementation("io.coil-kt.coil3:coil-compose:3.4.0")
+    implementation("com.squareup.retrofit2:retrofit:3.0.0")
+    implementation("com.squareup.retrofit2:converter-gson:3.0.0")
     implementation(libs.compose.rich.editor)
 }
